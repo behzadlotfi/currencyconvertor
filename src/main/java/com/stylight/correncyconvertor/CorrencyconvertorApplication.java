d350d155d971d05b5c4d9f8e942c6b415333640d
admin <sgmntech@gmail.com>
2021-11-14 00:29:50 +0330

    first commit

---
+++ src/main/java/com/stylight/correncyconvertor/CorrencyconvertorApplication.java
@@ -0,0 +1,13 @@
+package com.stylight.correncyconvertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrencyconvertorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorrencyconvertorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stylight/correncyconvertor/CorrencyconvertorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stylight.correncyconvertor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrencyconvertorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
